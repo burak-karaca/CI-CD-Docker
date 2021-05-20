06e2b8a608883a7402b2143d4fef7e6777692adf
Karaca <burak.karaca@nokia.com>
2021-05-20 22:27:51 +0200

    commit last

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CI CD PRACTICE AWS..
+         NANKIERA 10
         </p>
         <a
           className="App-link"
