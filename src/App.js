6bd66cf7526e2bac67aa3c078b9343e85901cdfa
Karaca <burak.karaca@nokia.com>
2021-05-26 10:44:57 +0200

    commit new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         CI CD DEV
+         Continous delivery test
         </p>
         <a
           className="App-link"
